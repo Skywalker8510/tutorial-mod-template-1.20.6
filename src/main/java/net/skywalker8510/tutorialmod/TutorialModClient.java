627f03b6655ded356c22a8ebc8ac1f2e83c38a55
Skywalker8510 <91810623+Skywalker8510@users.noreply.github.com>
2024-05-31 20:49:59 -0500

    video 1

---
+++ src/main/java/net/skywalker8510/tutorialmod/TutorialModClient.java
@@ -0,0 +1,11 @@
+package net.skywalker8510.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
